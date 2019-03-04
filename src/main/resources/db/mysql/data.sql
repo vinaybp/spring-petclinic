0d74cbf641c12e6daa92498357d10c69e6714fee
vinaybp <48116602+vinaybp@users.noreply.github.com>
2019-03-04 19:47:16 +0530

    Update data.sql

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
+INSERT IGNORE INTO vets VALUES (1, 'Vinay', 'BP');
 INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
 INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
 INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega');
